b8d47ee32ee0f7bfcc01fc071baaf09c4e692511
Saikat <inbox.saikat@outlook.com>
2017-06-28 23:50:00 +0530

    Add cloud jars

--- src/main/java/app/JsonAdapter.java
+++ src/main/java/app/JsonAdapter.java
@@ -4,6 +4,11 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.XML;
 
+import com.google.cloud.storage.Bucket;
+import com.google.cloud.storage.BucketInfo;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+
 public class JsonAdapter {
 
 	public static int PRETTY_PRINT_INDENT_FACTOR = 4;
@@ -11,13 +16,27 @@ public class JsonAdapter {
 
 	public static void main(String[] args) {
 		
+		System.out.println("Creating a bucket on Google Cloud Storage...");
+		createBucket("test-buck-1");
+
 		try {
 			JSONObject xmlJSONObj = XML.toJSONObject(TEST_XML_STRING);
 			String jsonPrettyPrintString = xmlJSONObj.toString(PRETTY_PRINT_INDENT_FACTOR);
+			System.out.println("The converted JSON data is being printed...");
 			System.out.println(jsonPrettyPrintString);
 		} catch (JSONException je) {
 			System.out.println(je.toString());
 		}
+	}
+
+	public static void createBucket(String bucketName) {
+		// Instantiates a client
+		Storage storage = StorageOptions.getDefaultInstance().getService();
+
+		// Creates the new bucket
+		Bucket bucket = storage.create(BucketInfo.of(bucketName));
+
+		System.out.printf("Bucket %s created.%n", bucket.getName());
 	}
 
 }
